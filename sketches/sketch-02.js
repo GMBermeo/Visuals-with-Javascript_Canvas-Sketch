f942f6011f1e5aa7ab64bf295484d3065ab027b1
Guilherme Bermêo <gm.bermeo@hotmail.com>
2022-02-21 17:57:31 -0300

    Unit 3

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -1,19 +1,20 @@
+import { gerarCorAleatoria, gerarCorMaterial } from "../CoresUtil.js";
 const canvasSketch = require("canvas-sketch");
+const math = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
+
+console.log(gerarCorAleatoria());
+console.log(gerarCorMaterial());
+console.log(gerarCorMaterial("900"));
 
 const settings = {
   dimensions: [1080, 1080],
 };
 
-const degToRad = (degrees) => {
-  return (degrees / 180) * Math.PI;
-};
-
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = "white";
-    context.fillRect(0, 0, width, height);
-
     context.fillStyle = "black";
+    context.fillRect(0, 0, width, height);
 
     const cx = width * 0.5;
     const cy = height * 0.5;
@@ -28,7 +29,7 @@ const sketch = () => {
     const radius = width * 0.3;
 
     for (let i = 0; i < num; i++) {
-      const slice = degToRad(360 / num);
+      const slice = math.degToRad(360 / num);
       const angle = slice * i;
 
       x = cx + radius * Math.sin(angle);
@@ -38,10 +39,32 @@ const sketch = () => {
 
       context.translate(x, y);
       context.rotate(-angle);
+      context.scale(random.range(0.1, 2), random.range(0.2, 0.5));
 
       context.beginPath();
-      context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
+      context.fillStyle = "white";
       context.fill();
+      context.restore();
+
+      context.save();
+      context.translate(cx, cy);
+      context.rotate(-angle);
+
+      context.lineWidth = random.range(5, 20);
+
+      context.beginPath();
+      context.arc(
+        0,
+        0,
+        radius * random.range(0.7, 1.3),
+        slice * random.range(0, -8),
+        slice * random.range(1, 5)
+      );
+      context.strokeStyle = "white";
+
+      context.stroke();
+
       context.restore();
     }
   };
