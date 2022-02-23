8b8d2a53f14fe524ab160fdf2c198132a85c2a5d
Guilherme Bermêo <gm.bermeo@hotmail.com>
2022-02-23 11:11:03 -0300

    Unit 3 - Animate

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -3,7 +3,14 @@ const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [1080, 1080],
+  animate: true,
 };
+
+// // Função dummy de como animar frames formando loop
+// const animate = () => {
+//   console.log('domestika');
+//   requestAnimationFrame(animate);
+// }
 
 const sketch = ({ context, width, height }) => {
   const agents = [];
@@ -20,14 +27,16 @@ const sketch = ({ context, width, height }) => {
     context.fillRect(0, 0, width, height);
 
     agents.forEach((agent) => {
+      agent.update();
       agent.draw(context);
+      agent.bounce(width, height);
     });
   };
 };
 
 canvasSketch(sketch, settings);
 
-class Point {
+class Vector {
   constructor(x, y, radius) {
     this.x = x;
     this.y = y;
@@ -36,14 +45,31 @@ class Point {
 
 class Agent {
   constructor(x, y) {
-    this.pos = new Point(x, y);
-    this.radius = 10;
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
+    this.radius = random.range(4, 12);
   }
+  bounce(width, height) {
+    if (this.pos.x <= 0 || this.pos.x >= width) this.vel.x *= -1;
+    if (this.pos.y <= 0 || this.pos.y >= width) this.vel.y *= -1;
+  }
+
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }
+
   draw(context) {
-    context.fillStyle = "black";
+    context.save();
+    context.translate(this.pos.x, this.pos.y);
+
+    context.lineWidth = 1;
 
     context.beginPath();
-    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
     context.fill();
+    context.stroke();
+
+    context.restore();
   }
 }
