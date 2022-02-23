3332f06c644cf5c004849073befeeda0df823cae
Guilherme Bermêo <gm.bermeo@hotmail.com>
2022-02-23 12:13:34 -0300

    Unit 4 - Sketch Agents Exercise 01

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -16,7 +16,7 @@ const settings = {
 const sketch = ({ context, width, height }) => {
   const agents = [];
 
-  for (let i = 0; i < 40; i++) {
+  for (let i = 0; i < 66; i++) {
     const x = random.range(0, width);
     const y = random.range(0, height);
 
