98f06cef69a18531fca7e0a2f535e1e4a430827c
Guilherme Bermêo <gm.bermeo@hotmail.com>
2022-02-23 11:34:11 -0300

    Unit 3 - Lines
    Here you will learn to draw lines between the agents, about optimization, and to use Pythagoras' theorem to find the distance between agents.
    https://www.mathplanet.com/education/pre-algebra/right-triangles-and-algebra/the-pythagorean-theorem

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -1,5 +1,6 @@
 const canvasSketch = require("canvas-sketch");
 const random = require("canvas-sketch-util/random");
+const math = require("canvas-sketch-util/math");
 
 const settings = {
   dimensions: [1080, 1080],
@@ -23,8 +24,27 @@ const sketch = ({ context, width, height }) => {
   }
 
   return ({ context, width, height }) => {
-    context.fillStyle = "white";
+    context.fillStyle = "black";
     context.fillRect(0, 0, width, height);
+
+    for (let i = 0; i < agents.length; i++) {
+      const agent = agents[i];
+      for (let j = i + 1; j < agents.length; j++) {
+        const other = agents[j];
+
+        const dist = agent.pos.getDistance(other.pos);
+
+        if (dist > 200) continue;
+
+        context.lineWidth = math.mapRange(dist, 0, 200, 4, 0.01);
+
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.lineTo(other.pos.x, other.pos.y);
+        context.strokeStyle = "white";
+        context.stroke();
+      }
+    }
 
     agents.forEach((agent) => {
       agent.update();
@@ -40,6 +60,12 @@ class Vector {
   constructor(x, y, radius) {
     this.x = x;
     this.y = y;
+  }
+
+  getDistance(v) {
+    const dx = this.x - v.x;
+    const dy = this.y - v.y;
+    return Math.sqrt(dx * dx + dy * dy);
   }
 }
 
@@ -62,11 +88,11 @@ class Agent {
   draw(context) {
     context.save();
     context.translate(this.pos.x, this.pos.y);
-
-    context.lineWidth = 1;
+    context.lineWidth = 2;
 
     context.beginPath();
     context.arc(0, 0, this.radius, 0, Math.PI * 2);
+    context.strokeStyle = "white";
     context.fill();
     context.stroke();
 
