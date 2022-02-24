e7d0ae94f2fb3faa70a45107f364cffcafd7d273
Guilherme Bermêo <gm.bermeo@hotmail.com>
2022-02-24 14:24:15 -0300

    Unit 5 - Grid

---
+++ sketches/sketch-04.js
@@ -0,0 +1,49 @@
+const canvasSketch = require("canvas-sketch");
+
+const settings = {
+  dimensions: [1080 * 2, 1080 * 2],
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, width, height);
+
+    const cols = 10;
+    const rows = 10;
+    const numCells = cols * rows;
+
+    const gridw = width * 0.8;
+    const gridh = height * 0.8;
+    const cellw = gridw / cols;
+    const cellh = gridh / rows;
+    const margx = (width - gridw) / 2;
+    const margy = (width - gridh) / 2;
+
+    for (let i = 0; i < numCells; i++) {
+      const col = i % cols;
+      const row = Math.floor(i / cols);
+
+      const x = col * cellw;
+      const y = row * cellh;
+      const w = cellw * 0.8;
+      const h = cellh * 0.8;
+
+      context.save();
+      context.translate(x, y);
+      context.translate(margx, margy);
+      context.translate(cellw / 2, cellh / 2);
+
+      context.lineWidth = 8;
+
+      context.beginPath();
+      context.moveTo(w * -0.5, 0);
+      context.lineTo(w * 0.5, 0);
+      context.stroke();
+
+      context.restore();
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
