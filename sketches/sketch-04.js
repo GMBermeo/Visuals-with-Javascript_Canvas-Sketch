3c3e68944db4e2360ca0dae4b88e73e3c08f8340
Guilherme Bermêo <gm.bermeo@hotmail.com>
2022-02-24 15:05:49 -0300

    Unit 5 - Noise

--- sketches/sketch-04.js
+++ sketches/sketch-04.js
@@ -1,16 +1,20 @@
 const canvasSketch = require("canvas-sketch");
+const random = require("canvas-sketch-util");
+const math = require("canvas-sketch-util/math");
+const { noise2D } = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [1080 * 2, 1080 * 2],
+  animate: true,
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
+  return ({ context, width, height, frame }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    const cols = 10;
-    const rows = 10;
+    const cols = 25;
+    const rows = 25;
     const numCells = cols * rows;
 
     const gridw = width * 0.8;
@@ -29,12 +33,17 @@ const sketch = () => {
       const w = cellw * 0.8;
       const h = cellh * 0.8;
 
+      const n = noise2D(x + frame * 10, y, 0.0001);
+      const angle = n * Math.PI;
+      const scale = math.mapRange(n, -1, 1, 0.1, 30);
+
       context.save();
       context.translate(x, y);
       context.translate(margx, margy);
       context.translate(cellw / 2, cellh / 2);
+      context.rotate(angle);
 
-      context.lineWidth = 8;
+      context.lineWidth = scale;
 
       context.beginPath();
       context.moveTo(w * -0.5, 0);
