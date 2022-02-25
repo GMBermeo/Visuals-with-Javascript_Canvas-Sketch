be4426e6da8f8606f3cc12e152d15048b566897e
Guilherme Bermêo <gm.bermeo@hotmail.com>
2022-02-25 14:12:13 -0300

    Unit 6 - Bitmap (.getImageData(0, 0, cols, rows).data;)

--- sketches/sketch-05.js
+++ sketches/sketch-05.js
@@ -4,38 +4,82 @@ const settings = {
   dimensions: [2048 / 2, 2048 / 2],
 };
 let manager;
+
 let text = "G";
 let fontSize = 1200;
 let fontFamily = "serif";
 
-const sketch = () => {
+const typeCanvas = document.createElement("canvas");
+const typeContext = typeCanvas.getContext("2d");
+
+const sketch = ({ context, width, height }) => {
+  const cell = 20;
+  const cols = Math.floor(width / cell);
+  const rows = Math.floor(height / cell);
+  const numCells = cols * rows;
+
+  typeCanvas.width = cols;
+  typeCanvas.height = rows;
+
   return ({ context, width, height }) => {
-    context.fillStyle = "white";
-    context.fillRect(0, 0, width, height);
+    typeContext.fillStyle = "black";
+    typeContext.fillRect(0, 0, width, height);
 
-    context.fillStyle = "black";
-    context.font = `${fontSize}px ${fontFamily}`;
-    context.textBaseline = "top";
-    // context.textAlign = "center";
+    fontSize = cols;
 
-    const metrics = context.measureText(text);
+    typeContext.fillStyle = "white";
+    typeContext.font = `${fontSize}px ${fontFamily}`;
+    typeContext.textBaseline = "top";
+
+    const metrics = typeContext.measureText(text);
     const mx = metrics.actualBoundingBoxLeft * -1;
     const my = metrics.actualBoundingBoxAscent * -1;
     const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
     const mh = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxDescent;
 
-    const x = (width - mw) / 2 - mx;
-    const y = (width - mh) / 2 - my;
+    const tx = (cols - mw) / 2 - mx;
+    const ty = (rows - mh) / 2 - my;
 
-    context.save();
-    context.translate(x, y);
+    typeContext.save();
+    typeContext.translate(tx, ty);
 
-    context.beginPath();
-    context.rect(mx, my, mw, mh);
-    context.stroke();
+    typeContext.beginPath();
+    typeContext.rect(mx, my, mw, mh);
+    typeContext.stroke();
 
-    context.fillText(text, 0, 0);
-    context.restore();
+    typeContext.fillText(text, 0, 0);
+    typeContext.restore();
+
+    const typeData = typeContext.getImageData(0, 0, cols, rows).data;
+
+    // context.drawImage(typeCanvas, 0, 0);
+
+    for (let i = 0; i < numCells; i++) {
+      const col = i % cols;
+      const row = Math.floor(i / cols);
+
+      const x = col * cell;
+      const y = row * cell;
+
+      const r = typeData[i * 4 + 0];
+      const g = typeData[i * 4 + 1];
+      const b = typeData[i * 4 + 2];
+      const a = typeData[i * 4 + 3];
+
+      context.fillStyle = `rgb(${r},${g},${b},${a})`;
+
+      context.save();
+      context.translate(x, y);
+
+      context.translate(cell / 2, cell / 2);
+      // context.fillRect(0, 0, cell, cell);
+
+      context.beginPath();
+      context.arc(0, 0, cell / 2, 0, Math.PI * 2);
+      context.fill();
+
+      context.restore();
+    }
   };
 };
 const onKeyUp = (e) => {
