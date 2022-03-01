932f2a1692e577a5a91bfc22389c1b5bd58fcfa9
Guilherme Bermêo <gm.bermeo@hotmail.com>
2022-03-01 11:51:50 -0300

    Grid with random.range working.

--- sketches/sketch-01-finalProject.js
+++ sketches/sketch-01-finalProject.js
@@ -1,11 +1,15 @@
 const canvasSketch = require("canvas-sketch");
+const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [1920 * 2, 1920 * 2],
 };
 
-const matrix = 5;
-const gap = matrix * 50;
+const matrix = Math.floor(random.range(3, 10));
+// const matrix = 10;
+console.log(matrix);
+const gap = matrix * Math.floor(random.range(25, 50));
+// const gap = 0;
 
 const cols = matrix;
 const rows = matrix;
@@ -15,29 +19,36 @@ const sketch = () => {
     context.fillStyle = "white";
     // TASK (linha 33)
     // Invert the colors of the sketch. Use black for the background and white for the outlines.
-    context.fillStyle = "white";
+    context.fillStyle = "black";
     context.fillRect(0, 0, width, height);
-    context.lineWidth = width * 0.005;
+    // context.lineWidth = width * 0.005;
 
     // let gap = width * 0.03;
 
     const marginx = width * 0.05;
     const marginy = height * 0.05;
-    const off = width * 0.02;
-    let w = (width * 0.9) / cols - gap;
-    let h = (height * 0.9) / rows - gap;
+    // const off = width * 0.02;
+    let w = (width * 0.9) / cols - (gap * (cols - 1)) / cols;
+    console.log(w);
+    let h = (height * 0.9) / rows - (gap * (rows - 1)) / rows;
     let x, y;
 
     for (let i = 0; i < cols; i++) {
       for (let j = 0; j < rows; j++) {
         x = marginx + (w + gap) * i;
         y = marginy + (h + gap) * j;
+        console.log(marginx, marginy, gap, w);
+        console.log("X, Y : " + x, y);
 
+        context.save();
+        context.fillStyle = "red";
+        context.fillRect(marginx, marginy, w, h);
         context.beginPath();
         context.rect(x, y, w, h);
         // context.strokeStyle = "black";
-        context.fillStyle = "black";
+        context.fillStyle = "white";
         context.fill();
+        context.restore();
         // context.stroke();
         // context.shadowBlur = 200;
         // context.shadowColor = "#00000022";
